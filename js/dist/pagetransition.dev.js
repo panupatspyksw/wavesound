2d2fd8ffd22a6d933c547a19d727153efad41f11
panupat <67039010+panupatspyksw@users.noreply.github.com>
2020-08-30 10:11:52 -0700

    wavesound

---
+++ js/dist/pagetransition.dev.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function pageTransition() {
+  var tl = gsap.timeline(); // tl.to('ul.transition li', { duration: .5, scaleY: 1, transformOrigin: "bottom left", stagger: .2 })
+
+  tl.to('ul.transition li', {
+    duration: 1,
+    scaleY: 0,
+    transformOrigin: "bottom left",
+    stagger: .2,
+    delay: .5
+  });
+}
+
+pageTransition();
+
+function delay(n) {
+  n = n || 2000;
+  return new Promise(function (done) {
+    setTimeout(function () {
+      done();
+      start = true;
+      console.log("end");
+      document.body.style.overflow = "scroll";
+      document.body.style.overflowX = "hidden";
+    }, n);
+  });
+}
